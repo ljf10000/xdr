de38ed210351a37e4801e57099160bdd93b5a130
ljf10000 <l_j_f@163.com>
2017-10-30 18:00:58 +0800

    .

--- main.c
+++ main.c
@@ -21,6 +21,54 @@ static int usage(void)
     return -1;
 }
 
+#define EVENTS      (IN_CLOSE_WRITE|IN_MOVED_TO)
+#define EVENTSIZE   (sizeof(struct inotify_event) + NAME_MAX + 1)
+#define EVENTCOUNT  1024
+
+static char evbuf[EVENTCOUNT*EVENTSIZE];
+
+int handle(char *tlv_path, char *xdr_path, char *sha_path)
+{
+    struct inotify_event *ev;
+    char *p;
+    int fd, err, len;
+
+    fd = inotify_init1(IN_CLOEXEC);
+    if (fd<0) {
+        return -errno;
+    }
+    
+    err = inotify_add_watch(fd, tlv_path, EVENTS);
+    if (err<0) {
+        return -errno;
+    }
+
+    for (;;) {
+        len = read(fd, evbuf, sizeof(evbuf));
+        if (len == -1 && errno != EAGAIN) {
+            return -errno;
+        }
+
+        for (p=evbuf; p<evbuf+len; p+=sizeof(struct inotify_event) + ev->len) {
+            ev = (struct inotify_event *)p;
+
+            if (ev->mask & EVENTS) {
+                char tlv[1+OS_FILENAME_LEN] = {0};
+                char xdr[1+OS_FILENAME_LEN] = {0};
+                xpair_t pair = XPAIR_INITER(tlv, xdr, sha_path);
+
+                os_saprintf(tlv, "%s/%s", tlv_path, ev->name);
+                os_saprintf(xdr, "%s/%s", xdr_path, ev->name);
+                
+                err = tlv_to_xdr(&pair);
+                if (err<0) {
+                    // log
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     self = argv[0];
@@ -51,29 +99,7 @@ int main(int argc, char *argv[])
         return usage();
     }
 
-    char *tlv_path = argv[0];
-    char *xdr_path = argv[1];
-    char *sha_path = argv[2];
-
-    mv_t handle(const char *path, const char *file)
-    {
-        char tlv[1+OS_FILENAME_LEN] = {0};
-        char xdr[1+OS_FILENAME_LEN] = {0};
-        
-        os_snprintf(tlv, OS_FILENAME_LEN, "%s/%s", tlv_path, file);
-        os_snprintf(xdr, OS_FILENAME_LEN, "%s/%s", xdr_path, file);
-        
-        xpair_t pair = XPAIR_INITER(tlv, xdr, sha_path);
-
-        int err = tlv_to_xdr(&pair);
-        if (err<0) {
-            return mv2_go(err);
-        }
-
-        return mv2_ok;
-    }
-    
-    return os_fscan_dir(tlv_path, false, NULL, handle);
+    return handle(argv[0], argv[1], argv[2]);
 }
 
 /******************************************************************************/
