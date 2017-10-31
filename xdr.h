827834b99b4990db2ae15c8e8352e167e908135d
ljf10000 <l_j_f@163.com>
2017-10-31 18:23:49 +0800

    .

--- xdr.h
+++ xdr.h
@@ -1404,12 +1404,12 @@ tlv_record_to_xdr(tlv_record_t *r, xdr_buffer_t *x)
                 ops = tlv_ops(tlv);
 
                 if (ops->toxdr) {
-                    xdr_dprint("toxdr %d:%d ...", i, tlv->id);
+                    xdr_dprint("toxdr %d:%d ...", j, tlv->id);
                     err = (*ops->toxdr)(x, tlv);
                     if (err<0) {
                         return err;
                     }
-                    xdr_dprint("toxdr %d:%d ok.", i, tlv->id);
+                    xdr_dprint("toxdr %d:%d ok.", j, tlv->id);
                 }
             }
         }
