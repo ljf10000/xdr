5a8d449b34496bd001adc9c54060bacceddc3300
ljf10000 <l_j_f@163.com>
2017-11-01 17:50:53 +0800

    .

--- os.h
+++ os.h
@@ -580,7 +580,7 @@ os_mmap(char *file, size_t length, off_t offset, bool readonly)
     int mflag = readonly?MAP_PRIVATE:MAP_SHARED;
     int prot  = readonly?PROT_READ:(PROT_READ|PROT_WRITE);
 
-    int fd = open(file, fflag, 0x644);
+    int fd = open(file, fflag|O_CLOEXEC, 0x664);
     if (fd<0) {
         return NULL;
     }
