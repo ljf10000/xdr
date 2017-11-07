956274b9328021df4afbb1736406a7253d747109
ljf10000 <l_j_f@163.com>
2017-11-07 11:24:54 +0800

    .

--- tlv.h
+++ tlv.h
@@ -1067,8 +1067,8 @@ struct xparse {
     .name       = _name,                        \
     .namelen    = _namelen,                     \
     .path       = _path,                        \
-    .tlv        = XBUFFER_INITER(xpath_fullname(_path, PATH_TLV)),  \
-    .xdr        = XBUFFER_INITER(xpath_fullname(_path, PATH_XDR)),  \
+    .tlv        = XBUFFER_INITER((_path)[PATH_TLV].fullname),   \
+    .xdr        = XBUFFER_INITER((_path)[PATH_XDR].fullname),   \
 }   /* end */
 
 static inline int
