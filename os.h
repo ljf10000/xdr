f271bdacac3730b0dbf456c730f79541c908581f
ljf10000 <l_j_f@163.com>
2017-11-09 15:31:02 +0800

    .

--- os.h
+++ os.h
@@ -77,7 +77,7 @@
 /* just for sourceinsight */
 #define size_t          size_t
 /******************************************************************************/
-#include "inttypes.h"
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdarg.h>
 #include <stdlib.h>
