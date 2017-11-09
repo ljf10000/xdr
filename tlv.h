70ffb311e64f795b8d5dd5d10ce4ee95f9b003cd
ljf10000 <l_j_f@163.com>
2017-11-09 15:53:37 +0800

    .

--- tlv.h
+++ tlv.h
@@ -1674,14 +1674,14 @@ tlv_record_parse(tlv_record_t *r)
             tlv_dump(DUMP_STREAM, tlv);
         }
 
-        err = tlv_trace(tlv_check(parse, tlv), "tlv_check %d:%d", parse->count, r->count);
+        err = xdr_trace(tlv_check(parse, tlv), "tlv_check %d:%d", parse->count, r->count);
         if (err<0) {
             parse->st_tlv->error++;
             
             return err;
         }
 
-        err = tlv_trace(tlv_record_save(r, tlv), "tlv_record_save %d:%d", parse->count, r->count);
+        err = xdr_trace(tlv_record_save(r, tlv), "tlv_record_save %d:%d", parse->count, r->count);
         if (err<0) {
             parse->st_tlv->error++;
             
