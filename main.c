40374b777a4754f294e922ba97be843cf48a815a
ljf10000 <l_j_f@163.com>
2017-11-23 14:16:05 +0800

    .

--- main.c
+++ main.c
@@ -91,7 +91,7 @@ get_qb(uint64 id)
 }
 
 static nameflag_t opt[] = {
-    { .flag = OPT_CLI,          .name = "--cli",        .help = "cli mode, must supply env " ENV_XDR_FILE},
+    { .flag = OPT_CLI,          .name = "--cli",        .help = "cli mode[must supply env " ENV_XDR_FILE "]"},
     { .flag = OPT_IP6,          .name = "--ip6",        .help = "ipv6[not support now]"},
     { .flag = OPT_STRICT,       .name = "--strict",     .help = "strict check"},
     { .flag = OPT_MULTI,        .name = "--multi",      .help = "multi thread"},
@@ -131,9 +131,9 @@ usage(void)
     err = nameflag_usage(opt);
     
     os_println(__tab "ENV:");
-    os_println(__tab2 ENV_XDR_SLEEP  "=" SYMBOL_TO_STRING(XDR_USLEEP));
-    os_println(__tab2 ENV_XDR_QUE    "=" SYMBOL_TO_STRING(WORKER_COUNT));
-    os_println(__tab2 ENV_XDR_WORKER "=" SYMBOL_TO_STRING(QUE_COUNT));
+    os_println(__tab2 ENV_XDR_SLEEP  "=" SYMBOL_TO_STRING(XDR_USLEEP)   ", just for multi thread mode");
+    os_println(__tab2 ENV_XDR_QUE    "=" SYMBOL_TO_STRING(WORKER_COUNT) ", just for multi thread mode");
+    os_println(__tab2 ENV_XDR_WORKER "=" SYMBOL_TO_STRING(QUE_COUNT)    ", just for multi thread mode");
 
     return err;
 }
